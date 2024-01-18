27e5b3eb3220c19dcfe96c57c2b8807d921993c9
Бат-Эрдэнэ Хадбаатар <23LP6564@G301-CC0192.local>
2024-01-18 13:01:43 +0800

    init

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: "https://geld-income.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
